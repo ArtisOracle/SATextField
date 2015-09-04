29204e1cb3b339e708aef30c41383ff27ae2f5be
Stefan Arambasich <ArtisOracle@gmail.com>
2015-09-03 20:02:52 -0400

    Initial Commit

---
+++ SATextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SATextField
+//
+//  Created by Stefan Arambasich on 9/3/2015.
+//  Copyright (c) 2015 Stefan Arambasich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
